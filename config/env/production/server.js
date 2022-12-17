f61af3f6ad40710b1f7e5c132d0884da9fcb5259
Seiji Villafranca <villafrancasra@gmail.com>
2022-12-17 20:52:00 +0100

    modified yaml for actions

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
     host: env("HOST", "0.0.0.0"),
     port: env.int("PORT", 1337),
-    url: "https://strapi-on-azure.azurewebsites.net",
+    url: "https://angularph-api.azurewebsites.net",
     admin: {
-      url: "https://witty-sky-061465203.azurestaticapps.net",
+      url: "https://blue-island-06fc01600.2.azurestaticapps.net/",
       serveAdminPanel: false,
     },
 });
